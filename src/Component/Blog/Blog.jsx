f492f20aa6ce256430358f9264bf6a8301ee2182
shahriar-shakil01797 <shahriarshakil28@gmail.com>
2025-06-08 17:03:34 +0600

    shakil

--- src/Component/Blog/Blog.jsx
+++ src/Component/Blog/Blog.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { FaBookmark } from "react-icons/fa";
 
 const Blog = ({blog}) => {
    
     return (
-        <div>
-            <h1>{blog.id}</h1>
+        <div className='m-2'>
+            
             {/* <img src={blog.cover} alt="" />
             <h2>{blog.title}</h2> */}
             <div className="card bg-base-100 w-96 shadow-sm">
@@ -14,7 +15,19 @@ const Blog = ({blog}) => {
                     alt="Shoes" />
                 </figure>
                 <div className="card-body">
+
+                    <div className="author flex justify-around items-center">
+                        <h3>{blog.author}</h3>
+                        <img className='w-16' src={blog.author_img} alt="" />
+                        <FaBookmark  size={25}/>
+                    </div>
+
                     <h2 className="card-title">{blog.title}</h2>
+                        <div className='flex'>
+                            {
+                            blog.hashtags.map((hashtag=><p>{hashtag}</p>))
+                            }
+                        </div>
                     <div className="card-actions justify-end">
                     <button className="btn btn-primary">Mark as read</button>
                     </div>
